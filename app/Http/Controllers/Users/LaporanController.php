14bf53be20d91fdbcf11b7cb5ffbf99631a53a38
skywall212 <skywall212@gmail.com>
2026-01-12 10:26:40 -0500

    Update code short alamat di laporan admin dan users

--- app/Http/Controllers/Users/LaporanController.php
+++ app/Http/Controllers/Users/LaporanController.php
@@ -159,6 +159,30 @@ class LaporanController extends Controller
             $laporan[$wargaId]['total'] += $jumlah;
         }
 
-        return collect($laporan)->sortBy('nama')->values()->toArray();
+        //return collect($laporan)->sortBy('nama')->values()->toArray();
+        return collect($laporan)
+        ->sortBy(function ($item) {
+            // contoh alamat: G8/12
+            $alamat = $item['alamat'];
+
+            if (preg_match('/([A-Z]+)(\d+)\/(\d+)/i', $alamat, $m)) {
+                // $m[1] = huruf (G)
+                // $m[2] = blok (8, 9, dst)
+                // $m[3] = nomor rumah (1..20)
+
+                return sprintf(
+                    '%s-%03d-%03d',
+                    strtoupper($m[1]),
+                    (int) $m[2],
+                    (int) $m[3]
+                );
+            }
+
+            // fallback jika format tidak sesuai
+            return $alamat;
+        })
+        ->values()
+        ->toArray();
+
     }
 }
